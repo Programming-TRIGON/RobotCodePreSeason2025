ab9d87a41bbb5e2c013afb3aa7209e2db97535cc
ShmayaR <shmaya.rosenblatt@gmail.com>
2024-12-24 13:22:39 +0200

    added Transporter package and Transporter subsystem

---
+++ src/main/java/frc/robot/subsystems/Transporter/Trasnporter.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.Transporter;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Trasnporter extends SubsystemBase {
+    public Trasnporter() {
+    }
+}
+
