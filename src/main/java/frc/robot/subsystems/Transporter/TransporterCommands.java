72bbc8058e16676854df4564c9f2c2c70379362c
ShmayaR <shmaya.rosenblatt@gmail.com>
2024-12-26 22:59:30 +0200

    Update TransporterCommands.java

--- src/main/java/frc/robot/subsystems/Transporter/TransporterCommands.java
+++ src/main/java/frc/robot/subsystems/Transporter/TransporterCommands.java
@@ -5,18 +5,17 @@ import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.robot.RobotContainer;
 
 public class TransporterCommands {
+    public static Command getSetTransporterStateCommand(TransporterConstants.TransporterState targetState) {
+        return getSetMotorOutputCommand(targetState.targetMotorOutput);
+    }
+
     private static Command getSetMotorOutputCommand(double motorOutput) {
         return new FunctionalCommand(
-                () -> {
-                },
+                () -> RobotContainer.TRANSPORTER.setVoltageOutput(motorOutput) ,
                 () -> RobotContainer.TRANSPORTER.setVoltageOutput(motorOutput),
                 (interrupted) -> RobotContainer.TRANSPORTER.stopMotors(),
                 () -> false,
                 RobotContainer.TRANSPORTER
         );
-    }
-
-    public static Command getSetTransporterStateCommand(TransporterConstants.TransporterState targetState) {
-        return getSetMotorOutputCommand(targetState.targetMotorOutput);
     }
 }
